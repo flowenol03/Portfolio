b8994d5418f80b87a6268804b1a12029ceb9e60b
flowenol03 <prathameshkhandekar3@gmail.com>
2024-07-21 14:40:38 +0530

    inital commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css'; // Ensure styles are correctly applied
+import logo from '../assets/flowenol-removebg-preview.png'; // Go up one level from components to src, then into assets
+
 
 function Header() {
   return (
     <header className="Header">
-      <h1>HSEMAHTARP</h1>
+      <img src={logo} alt="Logo" className="Logo" />
       <nav aria-label="Main Navigation">
         <ul>
           <li><a href="#hero">Home</a></li>
