4c00e8939c363770de0587f11eef6c177c549b96
flowenol03 <prathameshkhandekar3@gmail.com>
2024-07-21 12:30:32 +0530

    inital commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Header.css'; // You can add styles here
+import './Header.css'; // Ensure styles are correctly applied
 
 function Header() {
   return (
     <header className="Header">
       <h1>HSEMAHTARP</h1>
-      <nav>
+      <nav aria-label="Main Navigation">
         <ul>
           <li><a href="#hero">Home</a></li>
           <li><a href="#about">About</a></li>
