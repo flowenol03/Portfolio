ab1824e57f2b4b0395d60b95ff4756da1146b41d
flowenol03 <prathameshkhandekar3@gmail.com>
2024-08-01 19:57:30 +0530

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -5,24 +5,26 @@ function About() {
   return (
     <section id="about" className="About">
       <h2>About Me</h2>
+      <br />
+      <br />
       <div className="About-content">
-        <strong>Who I am</strong>
+        <h3>Who I am</h3>
         <p>
           I'm Prathamesh Khandekar, a passionate and driven web developer who loves transforming creative ideas into functional and visually appealing web applications. With a strong foundation in HTML, CSS, JavaScript, and React, I thrive on the challenge of bringing unique designs and user experiences to life.
         </p>
-        <strong>My Journey</strong>
+        <h3>My Journey</h3>
         <p>
           From a young age, I’ve been fascinated by technology and how it can be used to solve problems and make our lives easier. My journey into web development began with a curiosity about how websites are built and a desire to create my own. Over time, this curiosity has evolved into a full-fledged career, and I've gained experience in various projects that have honed my skills and deepened my understanding of front-end development.
         </p>
-        <strong>Skills and Expertise</strong>
+        <h3>Skills and Expertise</h3>
         <p>HTML/CSS: Crafting responsive and visually appealing layouts with a focus on user experience.</p>
         <p>JavaScript: Adding interactivity and dynamic content to websites.</p>
         <p>React: Building complex user interfaces with a modern, component-based approach.</p>
-        <strong>What Drives Me</strong>
+        <h3>What Drives Me</h3>
         <p>
           What excites me the most about web development is the endless possibility for creativity and problem-solving. I’m constantly learning new technologies and techniques to stay ahead in this fast-evolving field. My goal is to deliver high-quality solutions that not only meet but exceed client expectations.
         </p>
-        <strong>Looking Ahead</strong>
+        <h3>Looking Ahead</h3>
         <p>
           I’m always on the lookout for new challenges and opportunities to grow. Whether it’s diving into new frameworks, exploring innovative design trends, or collaborating on exciting projects, I’m eager to continue learning and pushing the boundaries of what’s possible.
         </p>
