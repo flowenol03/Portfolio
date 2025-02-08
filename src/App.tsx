156499f72e7383f9cb4ff3517457fd15485461f7
flowenol03 <prathameshkhandekar3@gmail.com>
2025-02-08 16:27:45 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,23 @@ function App() {
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Navbar activeSection={activeSection} setActiveSection={setActiveSection} />
       <main className="flex-grow">
-        <section id="home" className="pt-20">
+        <section id="home" className="py-16">
           <Home />
         </section>
-        <section id="about" className="py-20">
+        <section id="about" className="py-6 bg-white-100 mt-[-40px]">
           <About />
         </section>
-        <section id="projects" className="py-20 bg-white">
+        <section id="projects" className="py-6 bg-white-50">
           <Projects />
         </section>
-        <section id="hobbies" className="py-20">
+        <section id="hobbies" className="py-6 bg-white-100">
           <Hobbies />
         </section>
-        <section id="contact" className="py-20 bg-white">
+        <section id="contact" className="py-6 bg-white-50">
           <Contact />
         </section>
       </main>
-      <Footer /> {/* Add Footer here */}
+      <Footer />
     </div>
   );
 }
